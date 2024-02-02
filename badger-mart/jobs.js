0e17801c64c5531ddc926e6ca6a7b6ccb5af639a
WilliamZhu870 <williamzhu870@gmail.com>
2024-02-02 15:31:13 -0600

    Finished HW1.

--- badger-mart/jobs.js
+++ badger-mart/jobs.js
@@ -2,4 +2,21 @@ function submitApplication(e) {
     e.preventDefault(); // You can ignore this; prevents the default form submission!
 
     // TODO: Alert the user of the job that they applied for!
+        let jobs = document.getElementsByName('job');
+        let job;
+        for (let jobElement of jobs) {
+            if (jobElement.checked) {
+                job = jobElement.value;
+            }
+        }
+        console.log(job);
+        if (job) {
+            alert("Thank you for applying to be a " + job + "!");
+        } else {
+            alert("Please select a job!");
+
+        }
 }
+document.getElementById("job-apply").addEventListener("click", () => {
+    submitApplication();
+});
--- badger-mart/order.js
+++ badger-mart/order.js
@@ -1,77 +1,95 @@
 const ITEMS = ["apple", "bagel", "coconut", "donut", "egg"]
 
 const SALES_TAX = {
-	AL:	0.040,
-	AK:	0.000,
-	AZ:	0.056,
-	AR:	0.065,
-	CA:	0.073,
-	CO:	0.029,
-	CT:	0.064,
-	DE:	0.000,
-	DC:	0.060,
-	FL:	0.060,
-	GA:	0.040,
-	HI:	0.040,
-	ID:	0.060,
-	IL:	0.063,
-	IN:	0.070,
-	IA:	0.060,
-	KS:	0.065,
-	KY:	0.060,
-	LA:	0.045,
-	ME:	0.055,
-	MD:	0.060,
-	MA:	0.063,
-	MI:	0.060,
-	MN:	0.069,
-	MS:	0.070,
-	MO:	0.042,
-	MT:	0.000,
-	NE:	0.055,
-	NV:	0.069,
-	NH:	0.000,
-	NJ:	0.066,
-	NM:	0.051,
-	NY:	0.040,
-	NC:	0.048,
-	ND:	0.050,
-	OH:	0.058,
-	OK:	0.045,
-	OR:	0.000,
-	PA:	0.060,
-	RI:	0.070,
-	SC:	0.060,
-	SD:	0.045,
-	TN:	0.070,
-	TX:	0.063,
-	UT:	0.061,
-	VT:	0.060,
-	VA:	0.053,
-	WA:	0.065,
-	WV:	0.060,
-	WI:	0.050,
-	WY:	0.040,
+	AL: 0.040,
+	AK: 0.000,
+	AZ: 0.056,
+	AR: 0.065,
+	CA: 0.073,
+	CO: 0.029,
+	CT: 0.064,
+	DE: 0.000,
+	DC: 0.060,
+	FL: 0.060,
+	GA: 0.040,
+	HI: 0.040,
+	ID: 0.060,
+	IL: 0.063,
+	IN: 0.070,
+	IA: 0.060,
+	KS: 0.065,
+	KY: 0.060,
+	LA: 0.045,
+	ME: 0.055,
+	MD: 0.060,
+	MA: 0.063,
+	MI: 0.060,
+	MN: 0.069,
+	MS: 0.070,
+	MO: 0.042,
+	MT: 0.000,
+	NE: 0.055,
+	NV: 0.069,
+	NH: 0.000,
+	NJ: 0.066,
+	NM: 0.051,
+	NY: 0.040,
+	NC: 0.048,
+	ND: 0.050,
+	OH: 0.058,
+	OK: 0.045,
+	OR: 0.000,
+	PA: 0.060,
+	RI: 0.070,
+	SC: 0.060,
+	SD: 0.045,
+	TN: 0.070,
+	TX: 0.063,
+	UT: 0.061,
+	VT: 0.060,
+	VA: 0.053,
+	WA: 0.065,
+	WV: 0.060,
+	WI: 0.050,
+	WY: 0.040,
 }
+
+let stateAbb;
 
 function roundMoney(num) {
 	return Math.round(num * 100) / 100;
 }
 
 function calculateSubtotal() {
-	return 0.00; // TODO calculateSubtotal
+	let total = 0;
+	for (let x of ITEMS){
+		const quantity = parseInt(document.getElementById(x +"-quantity").value);
+	    const price = parseFloat(document.getElementById(x +"-price").innerText);
+		
+		total += quantity * price;
+
+	}
+
+	return total; // TODO calculateSubtotal
 }
 
 function calculateSalesTax() {
-	return 0.00; // TODO calculateSalesTax
+	let total = 0;
+	total = roundMoney(calculateSubtotal() * getSalesTaxRateForState(stateAbb))	
+	return total; // TODO calculateSalesTax
 }
 
 function getSalesTaxRateForState(state) {
+	for (let x in SALES_TAX) {
+		if (state === x)
+			return SALES_TAX[x];
+	}
 	return 0.00; // TODO getSalesTaxRateForState
 }
 
 document.getElementById("btn-what-is-my-sales-tax").addEventListener("click", () => {
 	const state = document.getElementById("state-tax").value;
+	stateAbb = state;
 	alert("The sales tax rate for " + state + " is " + (getSalesTaxRateForState(state) * 100).toFixed(2) + "%");
 })
 
@@ -80,7 +98,14 @@ document.getElementById("btn-subtotal").addEventListener("click", () => {
 });
 
 document.getElementById("btn-sales-tax").addEventListener("click", () => {
+	const state = document.getElementById("state-tax").value;
+	stateAbb = state;
 	alert("Your sales tax is: $" + calculateSalesTax().toFixed(2));
 });
 
 // TODO Add an event listener to btn-checkout
+document.getElementById("btn-checkout").addEventListener("click", () => {
+	const state = document.getElementById("state-tax").value;
+	stateAbb = state;
+	alert("Your total is: $" + (calculateSubtotal() + calculateSalesTax()));
+});
